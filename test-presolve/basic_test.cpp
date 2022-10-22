1c516f94c7a6861a364ad16b226a2a15122ea4c4
Josep Garcia-Reyero Sais <josepreyero@gmail.com>
2022-10-22 17:51:30 +0100

    Basic test case read one problem.

--- test-presolve/basic_test.cpp
+++ test-presolve/basic_test.cpp
@@ -1,4 +1,5 @@
 #include "../lib/utils/reader.hpp"
+#include "Highs.h"
 // #include <vector>
 // #include <iostream>
 // // #include <stdint.h>
@@ -6,22 +7,21 @@
 
 int main() {
   utils::Reader reader_;
-  //reader_.readFirstProblem("/home/maxguy/projects/hons/hons-project/problems/feasibility_testcases.txt");
-  // reader_.readProblem("/Users/pepe/hons-project/problems/feasibility_testcases.txt", 3);
-  // std::vector<std::vector<int16_t>> mat = reader_.problem_matrix_;
-  // std::vector<int16_t> max = reader_.upper_bounds_;
-  // std::vector<int16_t> min = reader_.lower_bounds_;
+  reader_.readProblem("/Users/pepe/hons-project/problems/feasibility_testcases.txt", 1);
+  std::vector<std::vector<int>> mat = reader_.problem_matrix_;
+  std::vector<int> max = reader_.upper_bounds_;
+  std::vector<int> min = reader_.lower_bounds_;
 
 
-  // for(int i =0; i < mat.size(); ++i){
-  //   for(int j = 0; j < mat[1].size(); ++j){
-  //     std::cout << mat[i][j] << std::endl;
-  //   }
-  // }
+  for(int i =0; i < mat.size(); ++i){
+    for(int j = 0; j < mat[1].size(); ++j){
+      std::cout << mat[i][j] << std::endl;
+    }
+  }
 
-  // for(int i = 0; i < max.size(); ++i){
-  //   std::cout << max.at(i) << ", " << min.at(i) << std::endl;
-  // }
+  for(int i = 0; i < max.size(); ++i){
+    std::cout << max.at(i) << ", " << min.at(i) << std::endl;
+  }
 
 
   return 0;
