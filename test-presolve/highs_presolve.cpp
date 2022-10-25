cd3d6353209f894c4549ba295705627909685f68
Josep Garcia-Reyero Sais <josepreyero@gmail.com>
2022-10-25 13:36:43 +0100

    Renamed files for clarity.

--- test-presolve/highs_presolve.cpp
+++ test-presolve/highs_presolve.cpp
@@ -1,39 +1,41 @@
-#include "Highs.h"
 #include "../lib/utils/reader.hpp"
+#include "Highs.h"
 #include <vector>
 #include <iostream>
-// #include <stdint.h>
 #include <string>
-#include "../lib/core/types.hpp"
 
+
+// Constants:
+std::string test_problems = "/Users/pepe/hons-project/problems/feasibility_testcases.txt";
 const int all_test_cases_count = 150218;
+const double inf = kHighsInf;
+
+// Parameters:
 const bool test_all = false;
-const int to_test_count = 1;
+const int to_test_count = 10;
+bool print_problems = true;
+
+// Report variables:
 int reduced_to_empty_count = 0;
 std::vector<string> used_presolve_rules;
 
 
-// HiGHS variables:
-const double inf = kHighsInf;
-
-
-/// @brief Reads and presolves one problem from the test cases
-/// using reader.cpp and HiGHS. 
-/// @param highs - HiGHS object.
-/// @param reader_ - Reader object.
-/// @param problem_number is the problem to presolve.
-void presolveProblem(
-  Highs& highs,
+/// @brief Prints a test case problem. 
+/// @param problem_matrix - matrix for the problem constraints.
+/// @param lower_bounds - lower bounds for problem constraints.
+/// @param upper_bounds - upper bounds for problem constraints.
+void printLP(
   const std::vector<std::vector<int>> problem_matrix,
   const std::vector<int> lower_bounds,
   const std::vector<int> upper_bounds
 );
 
 
-/// @brief For test problems 1 to N reads the problems using reader.cpp
-/// and applies and reports presolve using HiGHS.
-/// @param n the number of problems to read and presolve.
-void presolveProblemsN(const int n);
+/// @brief Returns whether a presolve rule has been used.
+/// @param presolve_rule 
+/// @return Boolean indicating whether presolve_rule has been
+/// found in used rules.
+bool findPresolveRuleInUsed(const std::string presolve_rule);
 
 
 /// @brief Defines a problem in a HiGHS instance.
@@ -53,65 +55,91 @@ HighsStatus defineLp(
 void reportAndLogPresolveLog(Highs& highs);
 
 
-/// @brief Returns the bounds to be entered to HiGHS.
-/// @param constant_term is the constant term of the constraint.
-/// @param upper_bound is the upper bound of the constraint.
-/// @param lower_bound is the lower bound of the constraint.
-/// @return double vector with lowe and upper bound. Both 0 if
-/// <upper_bound> = <lower_bound>, [-<constant_term>, inf] otherwise.
-std::vector<double> getHighsRowBounds(
-  const int constant_term,
-  const int lower_bound,
-  const int upper_bound
+/// @brief Reads and presolves one problem from the test cases
+/// using reader.cpp and HiGHS. 
+/// @param highs - HiGHS object.
+/// @param problem_matrix - matrix for the problem constraints.
+/// @param lower_bounds - lower bounds for problem constraints.
+/// @param upper_bounds - upper bounds for problem constraints.
+void presolveSingleProblem(
+  Highs& highs,
+  const std::vector<std::vector<int>> problem_matrix,
+  const std::vector<int> lower_bounds,
+  const std::vector<int> upper_bounds
 );
+
+
+/// @brief For test problems 1 to to problems_count reads the problems
+/// using reader.cpp and applies and reports presolve using HiGHS.
+/// @param n the number of problems to read and presolve.
+void presolveProblems(const int problems_count);
+
+
+void printLP(
+  const std::vector<std::vector<int>> problem_matrix,
+  const std::vector<int> lower_bounds,
+  const std::vector<int> upper_bounds
+) {
+    for (int i = 0; i < problem_matrix.size(); ++i) {
+      printf("Row %d \n", i+1);
+      for (int j = 0; j < problem_matrix[1].size(); ++j) {
+        std::cout << problem_matrix[i][j] << std::endl;
+      }
+      std::cout<<" "<<std::endl;
+    }
+
+    std::cout<<" "<<std::endl;
+    std::cout<<"Bounds"<<std::endl;;
+
+    for (int i = 0; i < problem_matrix.size(); ++i) {
+      std::string upper_bound = std::to_string(upper_bounds[i]);
+      if (upper_bound == "32765") {
+        upper_bound = "Inf";
+      }
+      std::cout << lower_bounds.at(i) << ", " << upper_bound << std::endl;
+    }
+}
+
+
+bool findPresolveRuleInUsed(const std::string presolve_rule) {
+  for (auto&used_rule : used_presolve_rules) {
+    if (used_rule == presolve_rule) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 
 void reportAndLogPresolveLog(Highs& highs) {
   const HighsPresolveLog& presolve_log = highs.getPresolveLog();
   // The presolve_rule_off option will alow certain presolve rules to
   // be switched off
-  int presolve_rule_off = highs.getOptions().presolve_rule_off;
-  int bit = 1;
-  printf("\nRule  Bit| Call Row Col| Name\n");
 
+  if (print_problems) {
+    printf("\nRule  Bit| Call Row Col| Name\n");
+  }
+  int bit = 1;
   for (int rule_ix = 0; rule_ix < kPresolveRuleCount; rule_ix++) {
-    bool allow = !(presolve_rule_off & bit);
     const HighsPresolveRuleLog& log = presolve_log.rule[rule_ix];
     const std::string presolve_rule = highs.presolveRuleTypeToString(rule_ix);
 
-    if (log.call) printf("  %2d %4d|  %3d %3d %3d| %s\n", rule_ix, bit,
-			 log.call, 
-			 log.row_removed,
-			 log.col_removed,
-			 presolve_rule.c_str());
-    bit *= 2;
+    if (log.call) {
+      if (print_problems) {
+        printf("  %2d %4d|  %3d %3d %3d| %s\n", rule_ix, bit,
+        log.call, 
+        log.row_removed,
+        log.col_removed,
+        presolve_rule.c_str());
+      }
 
-    if (std::find(used_presolve_rules.begin(), used_presolve_rules.end(), presolve_rule) != used_presolve_rules.end()) {
-      used_presolve_rules.push_back(presolve_rule);
+      if (!findPresolveRuleInUsed(presolve_rule)) {
+        used_presolve_rules.push_back(presolve_rule);
+      }
     }
+    bit *= 2; 
   }
-};
-
-
-std::vector<double> getHighsRowBounds(
-  const int constant_term,
-  const int lower_bound,
-  const int upper_bound
-) {
-  std::vector<double> highs_bounds; 
-
-  if ((upper_bound == 0) && lower_bound == 0) {
-     highs_bounds.push_back((double)(-constant_term));
-     highs_bounds.push_back((double)(-constant_term));
-
-     return highs_bounds;
-  }
-
-  const double highs_lower_bound = (double)(-constant_term);
-  highs_bounds.push_back(highs_lower_bound);
-  highs_bounds.push_back(inf);
-
-  return highs_bounds;
 };
 
 
@@ -139,8 +167,11 @@ HighsStatus defineLp(
     std::vector<int> row = problem_matrix[row_num];
     int constant_term = row[0];
     int lower_bound = lower_bounds[row_num];
-    int upper_bound = upper_bounds[row_num];
-    std::vector<double> highs_bounds = getHighsRowBounds(constant_term, lower_bound, upper_bound);
+    int upper_bound;
+
+    if (upper_bounds[row_num] == 32765) {
+      upper_bound = inf;
+    }
 
     //non-zero indices and values.
     std::vector<int> indices;
@@ -158,14 +189,14 @@ HighsStatus defineLp(
     
     // Add row to HiGHS.
     int num_nz = indices.size();
-    return_status = highs.addRow(highs_bounds[0], highs_bounds[1], num_nz, &indices[0], &values[0]);
+    return_status = highs.addRow(lower_bound, upper_bound, num_nz, &indices[0], &values[0]);
   }
 
   return return_status;
 };
 
 
-void presolveProblem(
+void presolveSingleProblem(
   Highs& highs,
   const std::vector<std::vector<int>> problem_matrix,
   const std::vector<int> lower_bounds,
@@ -194,7 +225,7 @@ void presolveProblem(
 };
 
 
-void presolveProblemsN(int n) {
+void presolveProblems(int problems_count) {
   // Instantiate HiGHS and define variable for
   // HiGHS status.
   Highs highs;
@@ -204,35 +235,57 @@ void presolveProblemsN(int n) {
   return_status = highs.setOptionValue("output_flag", false);
   assert(return_status == HighsStatus::kOk);
   highs.setOptionValue("presolve_log_report", true);
-  highs.setOptionValue("doubleton_equation_sudoku_report", true);
 
   //Instanciate reader.
   utils::Reader reader;
-  std::string test_problems = "/Users/pepe/hons-project/problems/feasibility_testcases.txt";
 
-  for (int i = 0; i < n; ++i) {
+  for (int n = 0; n < problems_count; n++) {
     // Reading problem.
-    reader.readProblem(test_problems, i);
-    std::vector<std::vector<int>> mat = reader.problem_matrix_;
-    std::vector<int> max = reader.upper_bounds_;
-    std::vector<int> min = reader.lower_bounds_;
+    reader.readProblem(test_problems, n+1);
+    std::vector<std::vector<int>> problem_matrix = reader.problem_matrix_;
+    std::vector<int> upper_bounds = reader.upper_bounds_;
+    std::vector<int> lower_bounds = reader.lower_bounds_;
 
-    presolveProblem(highs, mat, min, max);
+
+    if (print_problems) {
+      std::cout<<" "<<std::endl;
+      std::cout<<" "<<std::endl;
+      printf("Problem %d \n", n+1);
+      std::cout<<" "<<std::endl;
+      printLP(problem_matrix, lower_bounds, upper_bounds);
+      std::cout<<" "<<std::endl;
+    }
+
+    presolveSingleProblem(highs, problem_matrix, lower_bounds, upper_bounds);
     highs.clearModel();
   }
 };
 
 
 int main() {
+  int problems_to_test;
+
   if (test_all) {
-    presolveProblemsN(all_test_cases_count);
+    problems_to_test = all_test_cases_count;
+    print_problems = false;
   } else {
-    presolveProblemsN(to_test_count);
+    problems_to_test = to_test_count;
   }
 
+  presolveProblems(problems_to_test);
+
+  std::cout<<" "<<std::endl;
+  std::cout<<" "<<std::endl;
+  std::cout<<"Presolve rules used:"<<std::endl;
+  std::cout<<""<<std::endl;
   for(int i = 0; i < used_presolve_rules.size(); ++i){
       std::cout << used_presolve_rules[i] << std::endl;
   }
 
+  std::cout<<" "<<std::endl;  
+  printf("Problems tested: %d \n", problems_to_test);
+  printf("Problems reduced to empty: %d \n", reduced_to_empty_count);
+  
+
   return 0;
-};
+}
