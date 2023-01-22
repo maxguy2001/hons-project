5a1d11cb75838b69ebc6e31c159783dc7afb6fd6
maxguy2001 <max.guy2001@gmail.com>
2023-01-22 16:47:55 +0000

    swatted a bug - splat!

--- lib/primal_simplex/main.hpp
+++ lib/primal_simplex/main.hpp
@@ -1,99 +1,103 @@
-#include <vector>
 #include <cstdint>
+#include <vector>
 
-namespace primal_simplex{
+namespace primal_simplex {
 
-  class PrimalSimplex
-  {
-  public:
-    PrimalSimplex();
+class PrimalSimplex {
+public:
+  PrimalSimplex();
 
-    /**
-     * @brief Set the table class member 
-     * 
-     * @param table simplex table
-     */
-    void setProblem(const std::vector<std::vector<float>> table);
+  /**
+   * @brief Set the table class member
+   *
+   * @param table simplex table
+   */
+  void setProblem(const std::vector<std::vector<float>> table);
 
-    /**
-     * @brief Set the initial simplex basis
-     * 
-     * @param basis initial basis
-     */
-    void setBasis(const std::vector<int> basis);
+  // TODO: get rid!
+  void printTable();
 
-    /**
-     * @brief solve the given problem
-     * 
-     */
-    void solveProblem();
+  // TODO: get rid!
+  void printBasis();
 
-    /**
-     * @brief prints solution to simplex problem to terminal
-     * 
-     */
-    void printSolution();
+  /**
+   * @brief Set the initial simplex basis
+   *
+   * @param basis initial basis
+   */
+  void setBasis(const std::vector<int> basis);
 
-  private:
+  /**
+   * @brief solve the given problem
+   *
+   */
+  void solveProblem();
 
-    //simplex table
-    std::vector<std::vector<float>> table_;
+  /**
+   * @brief prints solution to simplex problem to terminal
+   *
+   */
+  void printSolution();
 
-    //simplex basis. Order of basis must be preserved!
-    std::vector<int> basis_;
+  // simplex table
+  std::vector<std::vector<float>> table_;
 
-    /**
-     * @brief returns a column of the table as a vector. Useful for iterating over
-     * 
-     * @param column_index index of column to be extracted from table
-     * @return std::vector<float> 
-     */
-    std::vector<float> extractColumnFromTable(const int column_index);
+  // simplex basis. Order of basis must be preserved!
+  std::vector<int> basis_;
 
-    /**
-     * @brief returns the index of the smallest nonbasic entry in the objective function
-     * (first row of table_)
-     * 
-     * @return int 
-     */
-    int getPivotColumnIndex();
+  /**
+   * @brief returns a column of the table as a vector. Useful for iterating over
+   *
+   * @param column_index index of column to be extracted from table
+   * @return std::vector<float>
+   */
+  std::vector<float> extractColumnFromTable(const int column_index);
 
-    /**
-     * @brief returns index of pivot row in table_ based on the minimum value found 
-     * during ratio testing
-     * 
-     * @param pivot_column_index index found previously in getPivotColumnIndex() funtion
-     * @return int 
-     */
-    int getPivotRowIndex(const int pivot_column_index);
+  /**
+   * @brief returns the index of the smallest nonbasic entry in the objective
+   * function (first row of table_)
+   *
+   * @return int
+   */
+  int getPivotColumnIndex();
 
-    /**
-     * @brief switch basis_ based on pivot row and column row
-     * 
-     * @param pivot_row_index found in getPivotRowIndex()
-     * @param pivot_column_index found in getPivotColumnIndex()
-     */
-    void switchBasis(const int pivot_row_index, const int pivot_column_index);
+  /**
+   * @brief returns index of pivot row in table_ based on the minimum value
+   * found during ratio testing
+   *
+   * @param pivot_column_index index found previously in getPivotColumnIndex()
+   * funtion
+   * @return int
+   */
+  int getPivotRowIndex(const int pivot_column_index);
 
-    /**
-     * @brief constructs new table_ object, completing row operations to reduce required
-     * variable to basic
-     * 
-     * @param pivot_row_index 
-     * @param pivot_column_index 
-     */
-    void constructNewTable(const int pivot_row_index, const int pivot_column_index);
+  /**
+   * @brief switch basis_ based on pivot row and column row
+   *
+   * @param pivot_row_index found in getPivotRowIndex()
+   * @param pivot_column_index found in getPivotColumnIndex()
+   */
+  void switchBasis(const int pivot_row_index, const int pivot_column_index);
 
-    /**
-     * @brief checks if solution is optimal yet.
-     * Solution is optimal if all elements of objective funciton (first row of table_) are 
-     * all positive.
-     * 
-     * @return true 
-     * @return false 
-     */
-    bool checkOptimality();
+  /**
+   * @brief constructs new table_ object, completing row operations to reduce
+   * required variable to basic
+   *
+   * @param pivot_row_index
+   * @param pivot_column_index
+   */
+  void constructNewTable(const int pivot_row_index,
+                         const int pivot_column_index);
 
-  };
-    
-}
+  /**
+   * @brief checks if solution is optimal yet.
+   * Solution is optimal if all elements of objective funciton (first row of
+   * table_) are all positive.
+   *
+   * @return true
+   * @return false
+   */
+  bool checkOptimality();
+};
+
+} // namespace primal_simplex
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,76 @@
+#include "../lib/primal_simplex/main.hpp"
 #include "../lib/core/consts.hpp"
-#include "../lib/utils/reader.hpp"
 #include "../lib/utils/primal_reader.hpp"
-#include <vector>
+#include "../lib/utils/reader.hpp"
 #include <iostream>
+#include <vector>
 
-int main(){
+int main() {
   utils::PrimalReader reader_;
-  //reader_.readFirstProblem("/home/maxguy/projects/hons/hons-project/problems/feasibility_testcases.txt");
+  // reader_.readFirstProblem("/home/maxguy/projects/hons/hons-project/problems/feasibility_testcases.txt");
 
-  reader_.readProblem("/home/maxguy/projects/hons/hons-project/problems/primal_problems.txt", 2);
+  reader_.readProblem(
+      "/home/maxguy/projects/hons/hons-project/problems/primal_problems.txt",
+      1);
 
   std::vector<std::vector<int>> table = reader_.table_;
   std::vector<int> basis = reader_.basis_;
 
-
-  for(int i =0; i < table.size(); ++i){
-    for(int j = 0; j < table[1].size(); ++j){
-      std::cout << table[i][j] << std::endl;
+  std::vector<std::vector<float>> table_float;
+  std::vector<float> temp_vec;
+  for (int i = 0; i < table.size(); ++i) {
+    for (int j = 0; j < table[0].size(); ++j) {
+      temp_vec.push_back(static_cast<float>(table.at(i).at(j)));
     }
+    table_float.push_back(temp_vec);
+    temp_vec.clear();
   }
 
-  for(int i = 0; i < basis.size(); ++i){
-    std::cout << basis.at(i) << std::endl;
+  primal_simplex::PrimalSimplex solver_;
+  solver_.setProblem(table_float);
+  solver_.setBasis(basis);
+  solver_.solveProblem();
+
+  /*
+   // iter 0
+   int pivot_column_index = solver_.getPivotColumnIndex();
+   int pivot_row_index = solver_.getPivotRowIndex(pivot_column_index);
+   solver_.switchBasis(pivot_row_index, pivot_column_index);
+   solver_.constructNewTable(pivot_row_index, pivot_column_index);
+
+   if (solver_.checkOptimality()) {
+     std::cout << "optimal solition found";
+   }
+
+   // iter 1
+   pivot_column_index = solver_.getPivotColumnIndex();
+   // pivot row index wrong!
+   pivot_row_index = solver_.getPivotRowIndex(pivot_column_index);
+   solver_.switchBasis(pivot_row_index, pivot_column_index);
+   solver_.constructNewTable(pivot_row_index, pivot_column_index);
+   // solver_.printTable();
+   // solver_.printBasis();
+   std::cout << pivot_column_index << " " << pivot_row_index << std::endl;
+
+   if (solver_.checkOptimality()) {
+     std::cout << "optimal solition found";
+   }
+
+
+  // iter 2
+  pivot_column_index = solver_.getPivotColumnIndex();
+  pivot_row_index = solver_.getPivotRowIndex(pivot_column_index);
+  solver_.switchBasis(pivot_row_index, pivot_column_index);
+  solver_.constructNewTable(pivot_row_index, pivot_column_index);
+  // solver_.printTable();
+  // solver_.printBasis();
+
+  if (solver_.checkOptimality()) {
+    std::cout << "optimal solition found" << std::endl;
   }
+
+  solver_.printSolution();
+  */
 
   return 0;
 }
